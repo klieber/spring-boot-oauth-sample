7c4ed886e7a64583d73489990f34601a5bb9fc40
Kyle Lieber <klieber@wrberkley.com>
2016-04-25 14:18:45 -0500

    separate out auth server config

--- auth-server/src/main/java/com/example/AuthServerApplication.java
+++ auth-server/src/main/java/com/example/AuthServerApplication.java
@@ -3,24 +3,15 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,7 +19,6 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.security.KeyPair;
 import java.security.Principal;
 
 @SpringBootApplication
@@ -91,46 +81,6 @@ public class AuthServerApplication extends WebMvcConfigurerAdapter {
 		@Override
 		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 			auth.parentAuthenticationManager(authenticationManager);
-		}
-	}
-
-	@Configuration
-	@EnableAuthorizationServer
-	protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-
-		@Autowired
-    @SuppressWarnings("SpringJavaAutowiringInspection")
-		private AuthenticationManager authenticationManager;
-
-		@Bean
-		public JwtAccessTokenConverter jwtAccessTokenConverter() {
-			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-			KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource("keystore.jks"), "foobar".toCharArray()).getKeyPair("test");
-			converter.setKeyPair(keyPair);
-			return converter;
-		}
-
-		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			clients.inMemory()
-					.withClient("webapp")
-					.secret("secret")
-					.authorizedGrantTypes("authorization_code", "refresh_token", "password")
-          .accessTokenValiditySeconds(15)
-          .refreshTokenValiditySeconds(14400)
-          .scopes("read","write")
-          .autoApprove("read","write")
-          .autoApprove(true);
-		}
-
-		@Override
-		public void configure(AuthorizationServerEndpointsConfigurer endpoints)	throws Exception {
-			endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
-		}
-
-		@Override
-		public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-			oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
 		}
 	}
 }
---
+++ auth-server/src/main/java/com/example/config/AuthorizationServer.java
@@ -0,0 +1,56 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+  @Autowired
+  @SuppressWarnings("SpringJavaAutowiringInspection")
+  private AuthenticationManager authenticationManager;
+
+  @Bean
+  public JwtAccessTokenConverter jwtAccessTokenConverter() {
+    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+    KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource("keystore.jks"), "foobar".toCharArray()).getKeyPair("test");
+    converter.setKeyPair(keyPair);
+    return converter;
+  }
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients.inMemory()
+      .withClient("webapp")
+      .secret("secret")
+      .authorizedGrantTypes("authorization_code", "refresh_token", "password")
+      .accessTokenValiditySeconds(15)
+      .refreshTokenValiditySeconds(14400)
+      .scopes("read","write")
+      .autoApprove("read","write")
+      .autoApprove(true);
+  }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints)	throws Exception {
+    endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
+  }
+
+  @Override
+  public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+    oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+  }
+}
